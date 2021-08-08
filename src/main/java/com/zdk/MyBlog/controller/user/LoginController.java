7d64f9f3d8ee993a6f9db8bc58d2674ad4840368
hnist <369365576@qq.com>
2021-08-08 21:00:06 +0800

    登录流程完善

--- src/main/java/com/zdk/MyBlog/controller/user/LoginController.java
+++ src/main/java/com/zdk/MyBlog/controller/user/LoginController.java
@@ -23,7 +23,7 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Slf4j
 @Controller
-@RequestMapping(value ="/user")
+@RequestMapping(value = "/user")
 public class LoginController extends BaseController {
 
     @Autowired
@@ -33,56 +33,53 @@ public class LoginController extends BaseController {
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    @PostMapping(value = "/userLogin")
+    @PostMapping(value = "/login")
     @ResponseBody
     public ApiResponse login(@RequestParam(name = "username") String username,
                              @RequestParam(name = "password") String password,
                              @RequestParam(name = "rememberMe") @Nullable String remember, HttpServletRequest request) {
-        String ip=IpKit.getIpAddrByRequest(request);
+        String ip = IpKit.getIpAddrByRequest(request);
         //构造登录错误次数唯一缓存key
-        String userCountKey=WebConst.LOGIN_ERROR_COUNT+ip+username;
+        String userCountKey = WebConst.LOGIN_ERROR_COUNT + ip + username;
         //构造登录成功后用户信息唯一缓存key
-        String userInfoKey=WebConst.USERINFO+ip+username;
+        String userInfoKey = WebConst.USERINFO + ip + username;
 
         Integer loginErrorCount = redisUtil.getNumber(userCountKey);
-        if(isOk(loginErrorCount)&&loginErrorCount>=3){
+        if (isOk(loginErrorCount) && loginErrorCount >= 3) {
             return ApiResponse.fail("您输入密码已经错误超过3次，请10分钟后尝试");
         }
-        if(isOk(username)){
-            User user = userService.login(username);
-            if(user!=null){
-                if(passwordEncoder.matches(password, user.getPassword())){
-                    user.setLoginDate(DateUtil.now());
-                    userService.updateUserInfo(user);
-                    redisUtil.del(userCountKey);
-                    if(isOk(remember)){
-                        redisUtil.hset(WebConst.USERINFO,WebConst.LOGIN_SESSION_KEY, user, 3600);
-                    }else{
-                        redisUtil.hset(WebConst.USERINFO,WebConst.LOGIN_SESSION_KEY, user, -1);
-                    }
-                    return ApiResponse.success();
-                }else{
-                    loginErrorCount = redisUtil.getNumber(userCountKey);
-                    if(isOk(loginErrorCount)){
-                        redisUtil.setNumber(userCountKey,1,600);
-                    }else{
-                        redisUtil.incr(userCountKey, 1);
-                        if(redisUtil.getNumber(userCountKey)>=3){
-                            redisUtil.expire(userCountKey,600);
-                        }
-                    }
-                    return ApiResponse.fail("账号或密码错误,您还有"+(3-redisUtil.getNumber(userCountKey))+"次机会");
+        User user = userService.login(username);
+        if (user != null) {
+            if (passwordEncoder.matches(password, user.getPassword())) {
+                user.setLoginDate(DateUtil.now());
+                userService.updateUserInfo(user);
+                redisUtil.del(userCountKey);
+                if (isOk(remember)) {
+                    redisUtil.hset(WebConst.USERINFO, WebConst.LOGIN_SESSION_KEY, user, 3600);
+                } else {
+                    redisUtil.hset(WebConst.USERINFO, WebConst.LOGIN_SESSION_KEY, user, -1);
                 }
-            }else{
-                return ApiResponse.fail("该用户不存在");
+                return ApiResponse.success();
+            } else {
+                loginErrorCount = redisUtil.getNumber(userCountKey);
+                if (isOk(loginErrorCount)) {
+                    redisUtil.setNumber(userCountKey, 1, 600);
+                } else {
+                    redisUtil.incr(userCountKey, 1);
+                    if (redisUtil.getNumber(userCountKey) >= 3) {
+                        redisUtil.expire(userCountKey, 600);
+                    }
+                }
+                return ApiResponse.fail("账号或密码错误,您还有" + (3 - redisUtil.getNumber(userCountKey)) + "次机会");
             }
-        }else{
-            return ApiResponse.fail("请输入合法的用户名");
+        } else {
+            return ApiResponse.fail("该用户不存在");
         }
     }
+
     @GetMapping("/logout")
-    public String logout(){
-        redisUtil.hdel(WebConst.USERINFO,WebConst.LOGIN_SESSION_KEY);
+    public String logout() {
+        redisUtil.hdel(WebConst.USERINFO, WebConst.LOGIN_SESSION_KEY);
         return "/login";
     }
 }
