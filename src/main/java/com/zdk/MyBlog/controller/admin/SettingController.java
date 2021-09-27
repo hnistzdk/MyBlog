9d0ec7f6fcc56d60e589124a9fcaf0fd8ac3d6da
hnist <369365576@qq.com>
2021-09-27 16:25:20 +0800

    完成后台设置部分

--- src/main/java/com/zdk/MyBlog/controller/admin/SettingController.java
+++ src/main/java/com/zdk/MyBlog/controller/admin/SettingController.java
@@ -1,14 +1,25 @@
 package com.zdk.MyBlog.controller.admin;
 
+import com.github.pagehelper.PageInfo;
 import com.zdk.MyBlog.controller.BaseController;
+import com.zdk.MyBlog.model.pojo.Options;
 import com.zdk.MyBlog.service.logs.LogsService;
 import com.zdk.MyBlog.service.options.OptionsService;
+import com.zdk.MyBlog.utils.ApiResponse;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import sun.util.resources.es.CurrencyNames_es_UY;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author zdk
@@ -24,4 +35,28 @@ public class SettingController extends BaseController {
 
     @Autowired
     private LogsService logsService;
+
+    @ApiOperation("设置页")
+    @GetMapping("")
+    public String index(Model model){
+        List<Options> list = optionsService.list();
+        HashMap<String, String> options = new HashMap<>(16);
+        list.forEach(option->options.put(option.getName(), option.getValue()));
+        model.addAttribute("options",options);
+        return "admin/setting";
+    }
+
+    @ApiOperation("保存全局设置和个性化设置")
+    @PostMapping("/globalAndIndividual")
+    @ResponseBody
+    public ApiResponse saveGlobal(){
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        HashMap<String, String> param = new HashMap<>(6);
+        parameterMap.forEach((key,value)->{
+            if (value[0]!=null){
+                param.put(key,value[0]);
+            }
+        });
+        return optionsService.updateGlobalAndIndividualSetting(param);
+    }
 }
