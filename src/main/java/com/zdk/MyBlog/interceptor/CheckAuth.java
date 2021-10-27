da0123a1d427a8db686540cea7a38441aa2fd4f5
hnist <369365576@qq.com>
2021-10-27 21:46:16 +0800

    权限架构init

---
+++ src/main/java/com/zdk/MyBlog/interceptor/CheckAuth.java
@@ -0,0 +1,16 @@
+package com.zdk.MyBlog.interceptor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author zdk
+ * @date 2021/10/27 20:55
+ */
+@Target({ElementType.METHOD,ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CheckAuth {
+    String value() default "";
+}
