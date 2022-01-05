4658e248a76e39ed881becf7a7704cede25fd9e0
hnist <369365576@qq.com>
2022-01-05 17:38:49 +0800

    修改druid配置

---
+++ src/main/java/com/zdk/MyBlog/config/DruidConfig.java
@@ -0,0 +1,40 @@
+package com.zdk.MyBlog.config;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author zdk
+ * @date 2022/1/5 17:22
+ */
+@Configuration
+public class DruidConfig {
+    @Bean
+    public ServletRegistrationBean druidStatViewServlet() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+        Map<String, String> initParams = new HashMap<>();
+        //　可配的属性都在 StatViewServlet 和其父类下
+        initParams.put("loginUsername", "admin");
+        initParams.put("loginPassword", "zdk");
+        servletRegistrationBean.setInitParameters(initParams);
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean druidWebStatFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        Map<String, String> initParams = new HashMap<>();
+        initParams.put("exclusions", "*.js,*.css,/druid/*");
+        filterRegistrationBean.setInitParameters(initParams);
+        filterRegistrationBean.setUrlPatterns(Arrays.asList("/*"));
+        return filterRegistrationBean;
+    }
+}
