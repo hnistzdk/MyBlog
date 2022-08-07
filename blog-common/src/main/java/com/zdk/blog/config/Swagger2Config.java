4e76bd0cd7409498b901b9f576e6c3e5dde38a37
hnistzdk <369365576@qq.com>
2022-08-07 14:13:23 +0800

    增加RestTemplate配置与工具类;整理各模块重复依赖

--- blog-common/src/main/java/com/zdk/blog/config/Swagger2Config.java
+++ blog-common/src/main/java/com/zdk/blog/config/Swagger2Config.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
@@ -16,23 +18,27 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
  * @Date 2022/7/23 13:50
  */
 @Configuration
-@EnableSwagger2WebMvc
+@EnableOpenApi
 @SuppressWarnings("all")
 public class Swagger2Config {
     @Bean
-    public Docket getUserDocket(){
-        ApiInfo apiInfo=new ApiInfoBuilder()
-                .title("MyBlog")//api标题
-                .description("我的博客相关接口描述")//api描述
-                .version("1.0.0")//版本号
-                .contact("张迪凯")//本API负责人的联系信息
-                .build();
-        return new Docket(DocumentationType.SWAGGER_2)//文档类型（swagger2）
-                .apiInfo(apiInfo)//设置包含在json ResourceListing响应中的api元信息
-                .select()//启动用于api选择的构建器
-                .apis(RequestHandlerSelectors.basePackage("com.zdk.blog.api.controller"))//扫描接口的包
-                .paths(PathSelectors.any())//路径过滤器（扫描所有路径）
+    public Docket webApiConfig(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("webApi")
+                .apiInfo(webApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.zdk.blog.api.controller"))
                 .build();
     }
+
+    private ApiInfo webApiInfo() {
+        return new ApiInfoBuilder()
+                .title("MyBlog接口文档")
+                .description("博客相关接口文档")
+                .version("1.0")
+                .contact(new Contact("zdk", "https://github.com/hnistzdk", "369365576@qq.com"))
+                .build();
+    }
+
 //    http://localhost:8090/swagger-ui.html
 }
